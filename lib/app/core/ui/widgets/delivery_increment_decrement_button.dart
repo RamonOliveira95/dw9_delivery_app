99180bf87d5c7bf1b8ba1fc378571d897223de10
RamonOliveira95 <ramonsilvadeoliveira95@hotmail.com>
2023-02-04 15:53:00 -0300

    0.0.4

--- lib/app/core/ui/widgets/delivery_increment_decrement_button.dart
+++ lib/app/core/ui/widgets/delivery_increment_decrement_button.dart
@@ -6,6 +6,7 @@ import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
 
 class DeliveryIncrementDecrementButton extends StatelessWidget {
 
+  final bool _compact;
   final int amout;
   final VoidCallback incrementTap;
   final VoidCallback decrementTap;
@@ -15,11 +16,19 @@ class DeliveryIncrementDecrementButton extends StatelessWidget {
     required this.amout,
     required this.incrementTap,
     required this.decrementTap,
-  });
+  }) : _compact = false;
+
+  const DeliveryIncrementDecrementButton.compact({
+    super.key,
+    required this.amout,
+    required this.incrementTap,
+    required this.decrementTap,
+  }) : _compact = true;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: _compact ? EdgeInsets.all(5) : null,
       decoration: BoxDecoration(
         border: Border.all(color: Colors.grey),
         borderRadius: BorderRadius.circular(7),
@@ -34,14 +43,14 @@ class DeliveryIncrementDecrementButton extends StatelessWidget {
               child: Text(
                 '-',
                 style: context.textStyles.textMedium
-                    .copyWith(fontSize: 22, color: Colors.grey),
+                    .copyWith(fontSize: _compact ? 10 : 22, color: Colors.grey),
               ),
             ),
           ),
           Text(
             amout.toString(),
             style: context.textStyles.textRegular.copyWith(
-              fontSize: 17,
+              fontSize: _compact ? 13 : 17,
               color: context.colors.secondary,
             ),
           ),
@@ -52,7 +61,7 @@ class DeliveryIncrementDecrementButton extends StatelessWidget {
               child: Text(
                 '+',
                 style: context.textStyles.textMedium
-                    .copyWith(fontSize: 22, color: context.colors.secondary),
+                    .copyWith(fontSize: _compact ? 10 : 22, color: context.colors.secondary),
               ),
                      ),
            )
